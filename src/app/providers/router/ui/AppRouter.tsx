71f964504f81bd46ca26c9b593f8bb457a46555d
Vadim Eryshev <yeryshev@gmail.com>
2024-03-02 16:07:53 +0300

    add PageLoader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,24 +1,20 @@
 import { Suspense } from 'react';
-import { useTranslation } from 'react-i18next';
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
-const AppRouter = () => {
-    const { t } = useTranslation();
-
-    return (
-        <Suspense fallback={<div>{t('Loading...')}</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({ path, element }) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={<div className="page-wrapper">{element}</div>}
-                    />
-                ))}
-            </Routes>
-        </Suspense>
-    );
-};
+const AppRouter = () => (
+    <Suspense fallback={<PageLoader />}>
+        <Routes>
+            {Object.values(routeConfig).map(({ path, element }) => (
+                <Route
+                    key={path}
+                    path={path}
+                    element={<div className="page-wrapper">{element}</div>}
+                />
+            ))}
+        </Routes>
+    </Suspense>
+);
 
 export default AppRouter;
--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,10 +1,16 @@
+import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
+import cls from './NotFoundPage.module.scss';
 
-export const NotFoundPage = () => {
+interface NotFoundPageProps {
+    className?: string;
+}
+
+export const NotFoundPage = ({ className }: NotFoundPageProps) => {
     const { t } = useTranslation();
     return (
-        <div>
-            <h1>{t('Page not found')}</h1>
+        <div className={classNames(cls.NotFoundPage, {}, [className])}>
+            {t('Страница не найдена')}
         </div>
     );
 };
