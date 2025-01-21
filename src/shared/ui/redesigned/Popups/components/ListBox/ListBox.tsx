886c925706f58a3d9a56f3ea2b702605146ad581
Vadim Eryshev <yeryshev@gmail.com>
2025-01-21 20:49:27 +0300

    Refactor UI components; migrate to redesigned structure and remove deprecated files

--- src/shared/ui/redesigned/Popups/components/ListBox/ListBox.tsx
+++ src/shared/ui/redesigned/Popups/components/ListBox/ListBox.tsx
@@ -25,10 +25,6 @@ interface ListBoxProps {
     label?: string;
 }
 
-/**
- * Устарел, используем новые компоненты из папки redesigned
- * @deprecated
- */
 export function ListBox(props: ListBoxProps) {
     const {
         className,
@@ -41,7 +37,7 @@ export function ListBox(props: ListBoxProps) {
         label,
     } = props;
 
-    const optionsClasses = [mapDirectionClass[direction]];
+    const optionsClasses = [mapDirectionClass[direction], popupCls.menu];
 
     return (
         <HStack gap="4">
